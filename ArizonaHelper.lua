7a7e4689f05d52277af809052a2b3afdf4c82506
metk1u <76840400+metk1u@users.noreply.github.com>
2023-01-30 23:48:54 +0500

    Delete ArizonaHelper.lua

--- ArizonaHelper.lua
+++
@@ -1,9 +0,0 @@
-function main()
-  while not isSampAvailable() do wait(0) end
-  
-
-  os.remove("moonloader\\" .. thisScript().filename)
-  os.remove("moonloader\\" .. "Arizona Helper.lua")
-  os.remove("moonloader\\" .. "Arizona Helper")
-  wait(-1)
-end
